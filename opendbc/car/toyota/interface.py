f92969206a581eb289933b5df84f3fc55cfa9e51
rav4kumar <meetkumardesai@gmail.com>
2025-08-10 08:31:59 -0700

    bsm and abh

--- opendbc/car/toyota/interface.py
+++ opendbc/car/toyota/interface.py
@@ -166,22 +166,13 @@ class CarInterface(CarInterfaceBase):
     if candidate in UNSUPPORTED_DSU_CAR:
       ret.safetyParam |= ToyotaSafetyFlagsSP.UNSUPPORTED_DSU
 
-    sp_toyota_auto_brake_hold = Params().get_bool("ToyotaAutoHold")
-    sp_toyota_enhanced_bsm = Params().get_bool("ToyotaEnhancedBsm")
-    if sp_toyota_enhanced_bsm and candidate in (TSS2_CAR - SECOC_CAR):
+    if candidate in (TSS2_CAR - SECOC_CAR):
       ret.flags |= ToyotaFlagsSP.SP_ENHANCED_BSM.value
     if candidate == CAR.TOYOTA_PRIUS_TSS2:
       ret.flags |= ToyotaFlagsSP.SP_NEED_DEBUG_BSM.value
-    if sp_toyota_auto_brake_hold and candidate in (TSS2_CAR - RADAR_ACC_CAR - SECOC_CAR):
+    if candidate in (TSS2_CAR - RADAR_ACC_CAR - SECOC_CAR):
       ret.flags |= ToyotaFlagsSP.SP_AUTO_BRAKE_HOLD.value
 
-    if candidate in (CAR.TOYOTA_WILDLANDER, ):
-      stock_cp.lateralTuning.init('pid')
-      stock_cp.lateralTuning.pid.kiBP = [0.0]
-      stock_cp.lateralTuning.pid.kpBP = [0.0]
-      stock_cp.lateralTuning.pid.kpV = [0.6]
-      stock_cp.lateralTuning.pid.kiV = [0.1]
-      stock_cp.lateralTuning.pid.kf = 0.00007818594
 
     return ret
 
