7d36e3a225438d3e188e345d2b152920be15dec1
rav4kumar <meetkumardesai@gmail.com>
2025-08-10 08:38:35 -0700

    custom tune

--- opendbc/car/toyota/carcontroller.py
+++ opendbc/car/toyota/carcontroller.py
@@ -47,16 +47,14 @@ def get_long_tune(CP, params):
     if Params().get_bool("ToyotaTSS2Long"):
       if CP.carFingerprint == CAR.TOYOTA_RAV4_TSS2:
         #optimal for rav4
-        #kiBP = [2.,  12.,  20.,  27.]
-        #kiV = [.348, .20,  .17,  .10]
-        kiBP = [2., 5., ]
-        kiV = [0.5, 0.25]
+        kiBP = [0.,  2.,   12.,  20.,  27.]
+        kiV = [.34,  .35,  .20,  .17,  .10]
       else:
         #optimal for corolla
-        kiBP = [0.5,  27.]
-        kiV =  [.3, .1]
-        #kiBP = [2., 5.]
-        #kiV = [0.5, 0.23]
+        kiBP = [0.,  2.,  12.,  27.,  40]
+        kiV = [.34,  .35, .20,  .0995,  .0995]
+        #kiBP = [0.,   3.,   8.,   12.,  20.,  27.,  40.]
+        #kiV = [.35,  .3085,  .234,  .213,  .17,  .10,  .101]
     else:
       kiBP = [2., 5.]
       kiV = [0.5, 0.25]
